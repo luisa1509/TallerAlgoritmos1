205f0ef569d8bcfd5f2c253958c897048151a643
[NymaXxX] <[dulze8246@gmail.com]>
2019-09-05 09:37:14 -0500

    creacion de Clases

---
+++ src/JS/User.js
@@ -0,0 +1,41 @@
+class User{
+
+    constructor(name, password, email, cardNumber, adress, city, record){
+        this.name= name;
+        this.password= password;
+        this.email= email;
+        this.cardNumber= cardNumber;
+        this.adress= adress;
+        this.city= city;
+        this.record= record;
+    }
+
+    uRegister(){
+
+    }
+
+    uLogin(){
+
+    }
+
+    Search(){
+
+    }
+
+    Buy(){
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
